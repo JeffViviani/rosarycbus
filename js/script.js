02742f6c3245f6f61f7902014f064b4e3d50a2de
Jeff Viviani <vivianijeff@gmail.com>
2020-11-24 20:53:54 -0500

    Work on banner. Added translations. More sacred scripture.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
+var weeksSinceEpoc;
+
 window.onload = function() {
 	let clock = new Date();
-	const weeksSinceEpoc = Math.floor(Math.floor((clock.getTime() / 86400000)) / 7);
+	weeksSinceEpoc = Math.floor(Math.floor((clock.getTime() / 86400000)) / 7);
+	document.getElementById("translation-select").onchange = function() {
+		hideAllReadings();
+		loadReadings();
+	}
+	hideAllReadings();
+	loadReadings();
+}
+
+function loadReadings() {
 	let hiddenReadings = document.getElementsByClassName("rotation-readings");
 	for(let i=0; i<hiddenReadings.length; i++) {
-		let readingsToChoose = hiddenReadings[i].getElementsByTagName("div");
+		let translation = document.getElementById("translation-select").value;
+		console.log(translation);
+		let readingsToChoose = hiddenReadings[i].getElementsByClassName(translation);
 		let numChoices = readingsToChoose.length;
-		readingsToChoose[weeksSinceEpoc % numChoices].style.display = "block";
+		if(numChoices > 0) {
+			console.log(weeksSinceEpoc % numChoices);
+			readingsToChoose[weeksSinceEpoc % numChoices].style.display = "block";
+		}
+	}
+}
+
+function hideAllReadings() {
+	console.log("yay");
+	let hiddenReadings = document.querySelectorAll(".rotation-readings > div");
+	for(let i=0; i<hiddenReadings.length; i++) {
+		console.log("Hiding");
+		hiddenReadings[i].style.display = "none";
 	}
 }
