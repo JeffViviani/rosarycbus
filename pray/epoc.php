b1d8b667f620d41bfbeadc2b8868869199eb8362
Jeff Viviani <intern@ableat.com>
2020-12-26 11:07:34 -0500

    Overhaul to switch to PHP for scripture.

---
+++ pray/epoc.php
@@ -0,0 +1,19 @@
+<?php
+  $daysSinceEpoc = floor(time() / 86400);
+  $weeksSinceEpoc = floor($daysSinceEpoc / 7);
+
+  function numFilesInDir($dir)  {
+    $fileCount = 0;
+    $files = glob($dir . "*");
+    if ($files){
+      $fileCount = count($files);
+    }
+    return $fileCount;
+  }
+
+  function getScripture($dir, $weeksSinceEpoc) {
+    $numPassages = numFilesInDir($dir);
+    $todaysPassage = $weeksSinceEpoc % $numPassages;
+    return file_get_contents($dir . strval($todaysPassage) . '.txt');
+  }
+?>
